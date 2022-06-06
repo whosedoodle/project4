698796c8de6decc18a86c2cda0f6e029f60c1dbc
whosedoodle <103860262+whosedoodle@users.noreply.github.com>
2022-06-06 07:44:32 -0700

    Add files via upload

---
+++ INSERT2.sql
@@ -0,0 +1,107 @@
+insert into  customer(
+	customer_id,
+	first_name,
+	last_name,
+	email,
+	phone_number,
+	address
+) VALUES (
+	'Sarah',
+	'Circle',
+	'sarah@msn.com'
+	'+1 (555) 555-5555',
+	'233 Main Drive',
+);
+
+insert into  customer(
+	customer_id,
+	first_name,
+	last_name,
+	email,
+	phone_number,
+	address
+) VALUES (
+	'Brave',
+	'Nuro',
+	'doctor@msn.com'
+	'+1 (234) 555-5555',
+	'111 East Drive',
+);
+
+insert into car(
+	car_id,
+	make,
+	model,
+	year,
+	price,
+	avalability
+) VALUES (
+	'Nissan',
+	'Sentra',
+	'2018'
+	'34000',
+	True,
+);
+
+insert into car(
+	car_id,
+	make,
+	model,
+	year,
+	price,
+	avalability
+) VALUES (
+	'Ford',
+	'F150',
+	'2020'
+	'40000',
+	False
+);
+
+insert into salesperson(
+	salesperson_id,
+	first_name,
+	last_name,
+) VALUES (
+	'John',
+	'Stewart',
+);
+
+insert into salesperson(
+	salesperson_id,
+	first_name,
+	last_name,
+) VALUES (
+	'Matthew',
+	'Cutter',
+);
+
+insert into mechanic(
+	mechanic_id,
+	first_name,
+	last_name,
+) VALUES (
+	'Justin',
+	'Park',
+);
+
+insert into mechanic(
+	mechanic_id,
+	first_name,
+	last_name,
+) VALUES (
+	'Alice',
+	'Barge',
+);
+
+insert into service_ticket(
+	service_cost,
+) values (
+	'2000',
+);
+
+insert into service_ticket(
+	service_cost,
+) values (
+	'3000',
+);
